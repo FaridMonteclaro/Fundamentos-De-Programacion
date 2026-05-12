16806db7878f0d02272f926e808c6b1e6ebb59b8
FaridMonteclaro <a2213207068@alumnos.uat.edu.mx>
2026-05-11 23:04:53 -0600

    Update Frecuencias.java

--- Tarea 16/Java/Frecuencias.java
+++ Tarea 16/Java/Frecuencias.java
@@ -1,1 +1,39 @@
+package Fundamentos;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Frecuencias {
+
+    public static void main(String[] args) throws IOException {
+      
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        String cadena;
+        int[] frecuencias = new int[26];
+        int i, pos, maxFrecuencia, indiceMax;
+        char letraMax;
+
+        System.out.print("Ingresa una cadena: ");
+        cadena = entrada.readLine();
+
+        for (i = 0; i < cadena.length(); i++) {
+            pos = cadena.charAt(i) - 'a';
+            frecuencias[pos]++;
+        }
+
+        maxFrecuencia = frecuencias[0];
+        indiceMax = 0;
+
+        for (i = 1; i < 26; i++) {
+            if (frecuencias[i] > maxFrecuencia) {
+                maxFrecuencia = frecuencias[i];
+                indiceMax = i;
+            }
+        }
+
+        letraMax = (char) ('a' + indiceMax);
+
+        System.out.println(letraMax + " " + maxFrecuencia);
+    }
+}
